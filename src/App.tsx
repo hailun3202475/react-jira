73bcc0423fad85912bc77849158464be2a9d2c46
DESKTOP-KB7OJAG\hailu <hailun3202475@gmail.com>
2021-04-18 16:25:37 +0800

    fix32

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,8 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-            className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"
+        >
           Learn React
         </a>
       </header>
