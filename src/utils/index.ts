b1e9ec57ade4b93d6d65de891de06dbd9c386e12
DESKTOP-KB7OJAG\hailu <hailun3202475@gmail.com>
2021-05-01 23:40:17 +0800

    chore: useDebounce添加泛型

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -33,7 +33,7 @@ export const useMount = (callback: () => void) => {
 //   }
 // }
 
-export const useDebounce = (value: unknown, delay?: number): any => {
+export const useDebounce = <V>(value: V, delay?: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
   useEffect(() => {
     // 每次在value变化以后,设置一个定时器
